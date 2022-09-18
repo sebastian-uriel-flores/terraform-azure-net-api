7da9490e65f378dc0bcd9aeccfd1325d48ba9959
Sebastian Uriel Flores <s.flores@outlook.com.ar>
2022-09-18 11:55:57 -0300

    Actualizo Pipeline

--- Infrastructure/configuration.tf
+++ Infrastructure/configuration.tf
@@ -1,19 +1,28 @@
-variable "backend-resource-group-name" {
-  description = "Name of the resource group used by the backend"
-  type        = string
-  sensitive   = true  
-}
-
-variable "backend-storage-account-name" {
-  description = "Name of the storage account used by the backend"
-  type        = string
-  sensitive   = true  
-}
-
 variable "location" {
   description = "Location of the resources"
   type        = string
   sensitive   = false
   default     = "Brazil South"
+  nullable    = false
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  sensitive   = false
+  default     = "resource-group"
+}
+
+variable "sqlserver_administrator_login" {
+  description = "Username of the SQL Server Instance"
+  type        = string
+  sensitive   = true
+  nullable    = false
+}
+
+variable "sqlserver_administrator_login_password" {
+  description = "Password of the SQL Server Instance"
+  type        = string
+  sensitive   = true
   nullable    = false
 }
--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -6,9 +6,9 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = var.backend-resource-group-name
-    storage_account_name = var.backend-storage-account-name
-    container_name       = "tf-state"
+    resource_group_name  = "tf-backend-rg"
+    storage_account_name = "tfbackendstac01"
+    container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
 
@@ -17,63 +17,83 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "terra-az-sqldb" {
-  name     = "terra-az-sqldb-resourcegroup"
+resource "azurerm_resource_group" "demo" {
+  name     = var.resource_group_name
   location = var.location
+
+  tags = {
+    Scope = "Demo"
+  }
 }
 
-#resource "azurerm_storage_account" "terra-az-sqldb" {
-#  name                     = "terraazsqldbstorage"
-#  resource_group_name      = azurerm_resource_group.terra-az-sqldb.name
-#  location                 = azurerm_resource_group.terra-az-sqldb.location
-#  account_tier             = "Standard"
-#  account_replication_type = "LRS"
-#}
-#
-#resource "azurerm_mssql_server" "terra-az-sqldb" {
-#  name                         = "terra-az-sqldb-sqlserver"
-#  resource_group_name          = azurerm_resource_group.terra-az-sqldb.name
-#  location                     = azurerm_resource_group.terra-az-sqldb.location
-#  version                      = "12.0"
-#  administrator_login          = "__terra-az-sqldb-sqlserver-username__"
-#  administrator_login_password = "__terra-az-sqldb-sqlserver-password__"
-#}
-#
-#resource "azurerm_mssql_database" "terra-az-sqldb" {
-#  name           = "terra-az-sqldb-db"
-#  server_id      = azurerm_mssql_server.terra-az-sqldb.id
-#  collation      = "SQL_Latin1_General_CP1_CI_AS"
-#  max_size_gb    = 250
-#  read_scale     = false
-#  sku_name       = "S0"
-#  zone_redundant = false
-#
-#  tags = {
-#    Environment = "dev"
-#  }
-#}
-#
-#resource "azurerm_service_plan" "terra-az-sqldb" {
-#  name                = "terra-az-sqldb-serviceplan"
-#  location            = azurerm_resource_group.terra-az-sqldb.location
-#  resource_group_name = azurerm_resource_group.terra-az-sqldb.name
-#  os_type             = "Linux"
-#  sku_name            = "P1v2"
-#}
-#
-#
-#resource "azurerm_linux_web_app" "terra-az-sqldb" {
-#  name                = "terra-az-sqldb-linuxwebapp"
-#  resource_group_name = azurerm_resource_group.terra-az-sqldb.name
-#  location            = azurerm_service_plan.terra-az-sqldb.location
-#  service_plan_id     = azurerm_service_plan.terra-az-sqldb.id
-#
-#  site_config {
-#  }
-#
-#  connection_string {
-#    name  = "cnTareas"
-#    type  = "SQLAzure"
-#    value = "Server=tcp:${azurerm_mssql_server.terra-az-sqldb.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.terra-az-sqldb.name};User Id=${azurerm_mssql_server.terra-az-sqldb.administrator_login};Password=${azurerm_mssql_server.terra-az-sqldb.administrator_login_password};Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-#  }
-#}
+resource "azurerm_storage_account" "demo" {
+  name                     = "sqlserverstorage"
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    Scope = "Demo"
+  }
+}
+
+resource "azurerm_mssql_server" "demo" {
+  name                         = "sqlserver"
+  resource_group_name          = azurerm_resource_group.demo.name
+  location                     = azurerm_resource_group.demo.location
+  version                      = "12.0"
+  administrator_login          = var.sqlserver_administrator_login
+  administrator_login_password = var.sqlserver_administrator_login_password
+
+  tags = {
+    Scope = "Demo"
+  }
+}
+
+resource "azurerm_mssql_database" "demo" {
+  name           = "sqlserverdb"
+  server_id      = azurerm_mssql_server.demo.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_gb    = 250
+  read_scale     = false
+  sku_name       = "S0"
+  zone_redundant = false
+
+  tags = {
+    Scope = "Demo"
+  }
+}
+
+resource "azurerm_service_plan" "demo" {
+  name                = "webappserviceplan"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+
+  tags = {
+    Scope = "Demo"
+  }
+}
+
+
+resource "azurerm_linux_web_app" "demo" {
+  name                = "webapp"
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_service_plan.demo.location
+  service_plan_id     = azurerm_service_plan.demo.id
+
+  site_config {
+  }
+
+  connection_string {
+    name  = "cnTareas"
+    type  = "SQLAzure"
+    value = "Server=tcp:${azurerm_mssql_server.demo.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.demo.name};User Id=${azurerm_mssql_server.demo.administrator_login};Password=${azurerm_mssql_server.demo.administrator_login_password};Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
+
+  tags = {
+    Scope = "Demo"
+  }
+}
